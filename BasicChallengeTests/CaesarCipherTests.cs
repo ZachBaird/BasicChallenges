028acc65fce3a4f3a4a5bf2f8ddf2bae0d85812b
Zach <zdb1994@yahoo.com>
2020-12-02 20:35:25 -0500

    Add project files.

---
+++ BasicChallengeTests/CaesarCipherTests.cs
@@ -0,0 +1,58 @@
+using BasicChallenges.CaesarCipher;
+using Xunit;
+
+namespace BasicChallengeTests
+{
+    public sealed class CaesarCipherTests
+    {
+        [Fact]
+        public void CorrectlyEncodesCaesarBy3()
+        {
+            var (message, encodeFactor) = ("Caesar", 3);
+
+            var result = CaesarCipher.Encode(message, encodeFactor);
+
+            Assert.Equal("Fdhvdu", result);
+        }
+
+        [Fact]
+        public void CorrectlyEncodesHelloBy5()
+        {
+            var (message, encodeFactor) = ("Hello", 5);
+
+            var result = CaesarCipher.Encode(message, encodeFactor);
+
+            Assert.Equal("Mjqqt", result);
+        }
+
+        [Fact]
+        public void CorrectlyEncodesWithNonLetters()
+        {
+            var (message, encodeFactor) = ("Hello, world!", 1);
+
+            var result = CaesarCipher.Encode(message, encodeFactor);
+
+            Assert.Equal("Ifmmp, xpsme!", result);
+        }
+
+        [Fact]
+        public void CorrectlyEncodesZToA()
+        {
+            var (message, encodeFactor) = ("Z", 1);
+
+            var result = CaesarCipher.Encode(message, encodeFactor);
+
+            Assert.Equal("A", result);
+        }
+
+        [Fact]
+        public void CorrectlyEncodesAToZ()
+        {
+            var (message, encodeFactor) = ("A", -1);
+
+            var result = CaesarCipher.Encode(message, encodeFactor);
+
+            Assert.Equal("Z", result);
+        }
+    }
+}
---
+++ BasicChallenges/CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BasicChallenges.CaesarCipher
+{
+    public sealed class CaesarCipher
+    {
+        private static int GetAsciiValue(string letter) => Encoding.ASCII.GetBytes(letter).First();
+
+        private static string GetNewLetter(string letter, int encodeFactor)
+        {
+            int ascii = GetAsciiValue(letter);
+            byte[] shiftedAscii = new byte[] { Convert.ToByte(ascii + encodeFactor) };
+
+            int testAscii = GetAsciiValue(letter.ToUpper()) + encodeFactor;
+
+            if (testAscii < 65 || testAscii > 90)
+            {
+                int difference = testAscii < 65 ? 65 - testAscii : testAscii - 90;
+
+                // If our testAscii is less than 65, then we want to find the ascii backwards from 90 (Z).
+                // If our testAscii wasn't, then we want to find the ascii forwards from 65 (A)
+                int newAscii = (testAscii < 65) switch
+                {
+                    true => 91 - difference,
+                    false => 64 + difference
+                };
+
+                return Encoding.ASCII.GetString(new byte[] { Convert.ToByte(newAscii) });
+            }
+
+            return Encoding.ASCII.GetString(shiftedAscii);
+        }
+
+        private static char MapNewChars(char character, int encodeFactor)
+        {
+            if (char.IsLetter(character))
+            {
+                string newLetter = GetNewLetter(character.ToString(), encodeFactor);
+                return newLetter.ToCharArray().First();
+            }
+
+            return character;
+        }
+
+        public static string Encode(string stringToEncode, int encodeFactor)
+        {
+            char[] chars = stringToEncode.ToCharArray();
+
+            char[] updatedChars = chars.Select(c => MapNewChars(c, encodeFactor)).ToArray();
+
+            return string.Join(string.Empty, updatedChars);
+        }
+    }
+}
